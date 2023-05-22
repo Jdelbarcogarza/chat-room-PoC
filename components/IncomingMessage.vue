10d575daf820c3812b79b4cbc9dd3cc225cd2320
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-05-22 09:54:16 -0600

    Message bubbles accept dynamic data

---
+++ components/IncomingMessage.vue
@@ -0,0 +1,12 @@
+<template>
+	<li
+		class="bg-incomingMsgGreen py-1 px-3 rounded-xl rounded-bl w-fit text-white font-medium"
+	>
+		<p>{{ props.text }}</p>
+</li>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+	text: string;
+}>();
+</script>
---
+++ components/SendingMessage.vue
@@ -0,0 +1,11 @@
+<template>
+  <li class="bg-sendingMsgGray self-end py-1 px-3 rounded-xl rounded-br w-fit text-white font-medium">
+    <p>{{ props.text }}</p>
+  </li>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	text: string;
+}>();
+</script>
