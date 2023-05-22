0449564d3d2063e69321fcf3a9d396fac49ed3c0
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-05-22 15:40:43 -0600

    now avatar bot always appears on last incoming message

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,55 @@
 <template>
 	<!-- contenedor del chat -->
-	<div class="min-w-sm max-w-md bg-orange-200 h-96 overflow-y-auto">
+	<div class="min-w-sm max-w-md bg- white h-96 overflow-y-auto">
 		<!-- chat -->
-		<ul class="flex flex-col p-2" :ref="ul" id="chatList">
+		<ul class="flex flex-col p-2" id="chatList">
 			<!-- <ChatComponent :chat="messages" /> -->
 
 			<IncomingMessage text="Hola que onda" />
-			<SendingMessage class="self-end" text="nada todo bien" />
-			<IncomingMessage text="que bueno, yo ando haciendo el testing" />
-			<SendingMessage class="self-end" text="que padre!! Yo tambien ando probando cosas" />
-			<SendingMessage class="self-end" text="quiero que funcione bien por eso" />
-			<IncomingMessage text="si va a salir, hemos trabajado duro" />
+			<SendingMessage class="self-end mt-2" text="nada todo bien" />
+			<IncomingMessage
+				class="mt-3"
+				text="que bueno, yo ando haciendo el testing"
+			/>
+			<SendingMessage
+				class="mt-3 self-end"
+				text="que padre!! Yo tambien ando probando cosas"
+			/>
+			<SendingMessage
+				class="self-end"
+				text="quiero que funcione bien por eso"
+			/>
 
-			<template v-for="message in messages">
-				<li v-if="message.isIncomingMessage">
-					<IncomingMessage :text="message.text" />
+			<!-- asi es como deben de ir acomodados el avatar y el mensaje -->
+			<!-- <div class="flex">
+				<BotAvatar class="relative top-5 right-2.5" />
+				<IncomingMessage
+					class="mt-2 -ml-3"
+					text="si va a salir, hemos trabajado duro"
+				/>
+			</div> -->
+
+			<template v-for="(message, index) in messages">
+				<!-- si es el ultimo mensaje del bot, poner ahi su avatar -->
+				<li class="max-w-[80%]" v-if="message.isIncomingMessage && lastBotMessageIndex === index">
+					<div class="flex">
+
+						<BotAvatar class="relative top-5 right-2.5" />
+						<IncomingMessage
+						class="mt-2 -ml-3"
+						:text="message.text"
+						/>
+					</div>
+
 				</li>
 
-				<li v-else class="self-end">
+				<!-- si es un mensaje en medio de la conversación, solo desplegar eso  -->
+				<li v-else-if="message.isIncomingMessage">
+					<IncomingMessage :text="message.text" />
+				</li>
+				
+				<li v-else class="self-end max-w-[80%]">
+					<!-- checar si mensaje anterior es del otro jugador para agregar margin adicional -->
 					<SendingMessage :text="message.text" />
 				</li>
 			</template>
@@ -45,35 +77,55 @@
 <script setup lang="ts">
 import { Message } from "~/types/Message.type";
 
-// SCROLL INTO VIEW
-const ul = ref();
+/* TODO: Detectar cuando el último mensaje es del otro "jugador"
+para agregar padding adicional. 
+
+Mensajes continuos del mismo jugador tienen margin de mt-0.5.
+Mensajes de jugador A y jugadorB tienen más margin.
+*/
 
 function scrollToElement() {
-	ul.value?.(false);
 	let element = document.getElementById("chatList");
-	element?.scrollIntoView({behavior: "smooth", block: "end"});
+	element?.scrollIntoView({ behavior: "smooth", block: "end" });
 }
 
 const messages = ref<Message[]>([]);
-let numberOfMessages: number = 0;
+const numberOfMessages = ref<number>(0);
+const numberOfIncomingMessages = ref<number>(0);
+
+const lastBotMessageIndex = ref<number>(0)
+
+
+const isLastMessageMine = ref<boolean>(false);
 
 function addMessage(text: string) {
 	const newMessage: Message = { text: text, isIncomingMessage: false };
 	messages.value.push(newMessage);
-	numberOfMessages++;
+	numberOfMessages.value =  numberOfMessages.value + 1;
 
 	setTimeout(() => {
 		scrollToElement();
 	}, 100);
+
+	isLastMessageMine.value = false;
+	console.log("last bot index", lastBotMessageIndex.value)
+
+
 }
 
 function addMessageIncoming(text: string) {
 	const newMessage: Message = { text: text, isIncomingMessage: true };
 	messages.value.push(newMessage);
-	numberOfMessages++;
+	numberOfIncomingMessages.value = numberOfIncomingMessages.value + 1;
+
+	lastBotMessageIndex.value = numberOfMessages.value + numberOfIncomingMessages.value - 1
+
+	console.log("last bot index", lastBotMessageIndex.value)
 
 	setTimeout(() => {
 		scrollToElement();
 	}, 100);
+
+	isLastMessageMine.value = true;
 }
 </script>
