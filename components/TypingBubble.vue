c50dfc104b0699d85908dcd158d8e9c4046458e2
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-05-22 09:55:29 -0600

    added variable to control visibility of component

--- components/TypingBubble.vue
+++ components/TypingBubble.vue
@@ -1,5 +1,9 @@
 <template>
-	<div class="flex space-x-1 bg-white w-fit h-fit p-2 pt-2 rounded-full">
+	<!-- En lugar de utilizar hidde, podemos utilizar la propiedad de  collapse   -->
+	<div
+		:class="{ 'visible': props.isTyping, 'hidden': !props.isTyping }"
+		class="flex space-x-1 bg-white w-fit h-fit p-2 pt-2 rounded-full"
+	>
 		<div
 			class="bg-gray-300 w-2 h-2 rounded-full animate-bounce blue-circle"
 		></div>
@@ -12,14 +16,20 @@
 	</div>
 </template>
 
+<script setup lang="ts">
+const props = defineProps<{
+	isTyping: boolean;
+}>();
+</script>
 
 <style>
-	.blue-circle{
-		animation-delay: 0.1s;
-	}
-	.green-circle{
-		animation-delay: 0.2s;
-	}
-	.red-circle{
-		animation-delay: 0.3s;
-	}</style>
+.blue-circle {
+	animation-delay: 0.1s;
+}
+.green-circle {
+	animation-delay: 0.2s;
+}
+.red-circle {
+	animation-delay: 0.3s;
+}
+</style>
