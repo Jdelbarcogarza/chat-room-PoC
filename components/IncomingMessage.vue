9cedf0acd4e98fab2283dd96010a81b1e8d0dccd
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-05-22 10:46:33 -0600

    added margin top of 0.5

--- components/IncomingMessage.vue
+++ components/IncomingMessage.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="bg-incomingMsgGreen py-1 px-3 rounded-xl rounded-bl w-fit text-white font-medium"
+		class="bg-incomingMsgGreen mt-0.5 py-1 px-3 rounded-xl rounded-bl w-fit text-white font-medium"
 	>
 		<p>{{ props.text }}</p>
 </div>
--- components/SendingMessage.vue
+++ components/SendingMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-sendingMsgGray self-end py-1 px-3 rounded-xl rounded-br w-fit text-white font-medium">
+  <div class="bg-sendingMsgGray mt-0.5 py-1 px-3 rounded-xl rounded-br w-fit text-white font-medium">
     <p>{{ props.text }}</p>
   </div>
 </template>
